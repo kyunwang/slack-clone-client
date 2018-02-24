9e2fd0fbd0f0f6dd2d5f3a356249f242d613bace
Kangyun Wang <wky@kangyuns-mbp.home>
2018-02-24 20:53:40 +0100

    Registerform. Form validation and feedback

--- src/routes/Register.js
+++ src/routes/Register.js
@@ -1,65 +1,137 @@
-import React, { Component } from "react";
-import { Container, Header, Input, Button } from "semantic-ui-react";
-import gql from "graphql-tag";
-import { graphql } from "react-apollo";
+import React, { Component } from 'react';
+import { Container, Header, Input, Button, Message } from 'semantic-ui-react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class Register extends Component {
-  state = {
-    username: "",
-    email: "",
-    password: ""
-  };
+	state = {
+		username: '',
+		usernameError: '',
+		email: '',
+		emailError: '',
+		password: '',
+		passwordError: '',
+	};
 
-  onChange = e => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
+	onChange = e => {
+		const { name, value } = e.target;
 
-  onSubmit = async () => {
-    console.log("Submitted", this.state);
-    const response = await this.props.mutate({
-      variables: this.state
-    });
-    console.log(response);
-  };
+		// Set state property on basis of the input name
+		this.setState({ [name]: value });
+	};
 
-  render() {
-    const { username, email, password } = this.state;
+	onSubmit = async () => {
+		// Reset our errors
+		this.setState({
+			usernameError: '',
+			emailError: '',
+			passwordError: ''
+		});
 
-    return (
-      <Container text>
-        <Header as="h2">Register</Header>
-        <Input
-          name="username"
-          onChange={this.onChange}
-          value={username}
-          fluid
-          placeholder="Username"
-        />
-        <Input
-          name="email"
-          onChange={this.onChange}
-          value={email}
-          fluid
-          placeholder="Email"
-        />
-        <Input
-          name="password"
-          type="password"
-          onChange={this.onChange}
-          value={password}
-          fluid
-          placeholder="Password"
-        />
-        <Button onClick={this.onSubmit}>Submit</Button>
-      </Container>
-    );
-  }
+
+		const { username, email, password } = this.state;
+
+		// Mutate with the following variables
+		const response = await this.props.mutate({
+			variables: { username, email, password },
+		});
+
+		// Get the returned response from the mutation defined below
+		const { ok, errors } = response.data.register;
+		if (ok) {
+			this.props.history.push('/');
+		} else {
+			const err = {};
+			errors.forEach(({ path, message }) => {
+				err[`${path}Error`] = message;
+			});
+
+			this.setState(err);
+		}
+
+
+
+		console.log(response.data.register);
+	};
+
+	render() {
+		const {
+			username,
+			email,
+			password,
+			usernameError,
+			emailError,
+			passwordError
+		} = this.state;
+
+		// If there are errors push them to this list
+		const errorList = [];
+
+		if (usernameError) {
+			errorList.push(usernameError);
+		}
+		if (emailError) {
+			errorList.push(emailError);
+		}
+		if (passwordError) {
+			errorList.push(passwordError);
+		}
+
+
+		return (
+			<Container text>
+				<Header as="h2">Register</Header>
+				<Input
+					error={!!usernameError}
+					name="username"
+					onChange={this.onChange}
+					value={username}
+					fluid
+					placeholder="Username"
+				/>
+				<Input
+					error={!!emailError}
+					name="email"
+					onChange={this.onChange}
+					value={email}
+					fluid
+					placeholder="Email"
+				/>
+				<Input
+					error={!!passwordError}
+					name="password"
+					type="password"
+					onChange={this.onChange}
+					value={password}
+					fluid
+					placeholder="Password"
+				/>
+				<Button onClick={this.onSubmit}>Submit</Button>
+
+				{
+					(usernameError || emailError || passwordError) ?
+						<Message
+							error
+							header="There was some errors in your submission"
+							list={errorList}
+						/>
+						: null
+				}
+			</Container>
+		);
+	}
 }
 
+// Defining the varibales like $variable: String!
 const registerMutation = gql`
   mutation($username: String!, $email: String!, $password: String!) {
-    register(username: $username, email: $email, password: $password)
+    register(username: $username, email: $email, password: $password) {
+      ok
+      errors {
+        path
+        message
+      }
+    }
   }
 `;
 
