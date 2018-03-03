605a31be944aec5d9439ff7631ad39edea3aa295
Kangyun Wang <wky@kangyuns-mbp.home>
2018-03-03 22:06:51 +0100

    Styling sidebar with styled-components

--- src/components/Channels.js
+++ src/components/Channels.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+
 const ChannelsWrapper = styled.section`
 	grid-column: 2;
 	grid-row: 1 / 4;
@@ -9,34 +10,76 @@ const ChannelsWrapper = styled.section`
 	color: #958993;
 `;
 
-const channel = ({ id, name }) => (
-  <li key={`channel-${id}`}># {name}</li>
+const paddingLeft = 'padding-left: 10px';
+
+const PushLeft = styled.div`${paddingLeft}`;
+
+const TeamNameHeader = styled.h1`
+	color: #fff;
+	font-size: 1.5em;
+`;
+
+const SideBarList = styled.ul`
+	width: 100%;
+	list-style: none;
+	padding-left: 0;
+`;
+
+const SideBarHeaderList = styled.li`${paddingLeft};`;
+
+const SideBarListItem = styled.li`
+	${paddingLeft};
+	padding-top: 3px;
+	&:hover {
+		background: #3e313c;
+	}
+`;
+
+const Green = styled.span`color: #38973d;`;
+
+const Bubble = ({ on = false }) => (on ? <Green>●</Green> : '○');
+Bubble.propTypes = { on: PropTypes.bool.isRequired };
+
+const Channel = ({ id, name }) => (
+  <SideBarListItem key={`channel-${id}`}># {name}</SideBarListItem>
 );
 
-const user = ({ id, name }) => (
-  <li key={`user-${id}`}>{name}</li>
+Channel.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+const User = ({ id, name }) => (
+  <SideBarListItem key={`user-${id}`}>
+    <Bubble /> {name}
+  </SideBarListItem>
 );
+
+User.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+};
 
 function Channels({
   teamName, username, channels, users,
 }) {
   return (
     <ChannelsWrapper>
-      <div>
-        {teamName}
+      <PushLeft>
+        <TeamNameHeader>{teamName}</TeamNameHeader>
         {username}
+      </PushLeft>
+      <div>
+        <SideBarList>
+          <SideBarHeaderList>Channels</SideBarHeaderList>
+          {channels.map(Channel)}
+        </SideBarList>
       </div>
       <div>
-        <ul>
-          <li>Channels</li>
-          {channels.map(channel)}
-        </ul>
-      </div>
-      <div>
-        <ul>
-          <li>Direct messages</li>
-          {users.map(user)}
-        </ul>
+        <SideBarList>
+          <SideBarHeaderList>Direct messages</SideBarHeaderList>
+          {users.map(User)}
+        </SideBarList>
       </div>
     </ChannelsWrapper>
   );
--- src/components/Teams.js
+++ src/components/Teams.js
@@ -10,16 +10,49 @@ const TeamsWrapper = styled.section`
 	color: #958993;
 `;
 
-const team = ({ id, letter }) => (
-  <li key={`user-${id}`}>{letter}</li>
+const TeamList = styled.ul`
+	width: 100%;
+	padding-left: 0;
+	list-style: none;
+`;
+
+const TeamItem = styled.li`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	font-size: 1.5em;
+	color: #fff;
+	background-color: #676066;
+	height: 3rem;
+	width: 3rem;
+	
+	margin: auto;
+	margin-bottom: 10px;
+	border-radius: .5rem;
+
+	transition: .3s border ease-in;
+	&:hover {
+		border-style: solid;
+		border-width: thick;
+		border-color: #767676;
+	}
+`;
+
+const Team = ({ id, letter }) => (
+  <TeamItem key={`user-${id}`}>{letter}</TeamItem>
 );
+
+Team.propTypes = {
+  id: PropTypes.number.isRequired,
+  number: PropTypes.string.isRequired,
+};
 
 function Teams({ teams }) {
   return (
     <TeamsWrapper>
-      <ul>
-        {teams.map(team)}
-      </ul>
+      <TeamList>
+        {teams.map(Team)}
+      </TeamList>
     </TeamsWrapper>
   );
 }
