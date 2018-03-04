7d4016cc26a2f5358a02e2408cd2a49996b7a27c
Kangyun Wang <wky@kangyuns-mbp.home>
2018-03-04 21:26:11 +0100

    Moved teams, channels to sidebar. Query all teams and pass it to the moved comps

--- src/routes/ViewTeam.js
+++ src/routes/ViewTeam.js
@@ -1,34 +1,16 @@
 import React from 'react';
 
 import AppLayout from '../components/AppLayout';
-import Channels from '../components/Channels';
-import Teams from '../components/Teams';
 import Header from '../components/Header';
+import Sidebar from '../containers/Sidebar';
 import Messages from '../components/Messages';
 import SendMessage from '../components/SendMessage';
 
 function ViewTeam(props) {
   return (
     <AppLayout>
-      <Teams
-        teams={[
-			{ id: 1, letter: 'T' },
-			{ id: 2, letter: 'G' },
-		]}
-      />
-      <Channels
-        teamName="Team name"
-        username="username"
-        channels={[
-			  { id: 1, name: 'general' },
-			  { id: 2, name: 'random' },
-		  ]}
-        users={[
-			  { id: 1, name: 'slackbot' },
-			  { id: 2, name: 'user1' },
-		  ]}
-      />
       <Header channelName="General" />
+      <Sidebar currentTeamId={4} />
       <Messages>
         <ul className="message-list">
           <li />
